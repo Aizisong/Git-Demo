7707b1a9a38cd7821a420dfdc1628eb76fabe35c
Aizisong <2414291323@qq.com>
2023-04-18 17:22:35 +0800

    我他妈服了

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -4,6 +4,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("我爱你git");
         System.out.println("test change");
-        System.out.println("测试退");
+        System.out.println("我他妈就服了");
     }
 }
